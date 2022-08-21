5b93ed3a768c04d0874f1e90adcdbe4aae9c47c6
Patrik Lilja <lilja.patrik@gmail.com>
2022-08-21 11:37:41 +0200

    Add solution

---
+++ 1-fibonacci-number/1-fibonacci-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int a = 0; 
+        int b = 1;
+        for (int i = 2; i <= n; ++i) {
+            int next = a + b;
+            a = b;
+            b = next;
+        } 
+        return b;
+    }
+}
