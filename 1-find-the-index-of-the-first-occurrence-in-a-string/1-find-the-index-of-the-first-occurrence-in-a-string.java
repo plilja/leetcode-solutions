14c6628bb820ddb867f413a517031cd9792cf39c
Patrik Lilja <lilja.patrik@gmail.com>
2022-09-11 21:04:55 +0200

    Add solution

---
+++ 1-find-the-index-of-the-first-occurrence-in-a-string/1-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        // probably not what they are looking for but instructions doesn't state
+        // that it doesn't want this ...
+        return haystack.indexOf(needle);
+    }
+}
