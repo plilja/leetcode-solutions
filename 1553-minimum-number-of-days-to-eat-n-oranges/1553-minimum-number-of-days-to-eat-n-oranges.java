a443a06a01001ab73cbc7c9b0ef88cd60130ed29
Patrik Lilja <lilja.patrik@gmail.com>
2022-10-13 15:03:15 +0200

    Time: 6 ms (85.34%), Space: 42.9 MB (54.13%) - LeetHub

--- 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.java
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.java
@@ -1,46 +1,20 @@
 class Solution {
     public int minDays(int n) {
-        Map<Integer, Integer> visited = new HashMap<>();
-        PriorityQueue<Item> q = new PriorityQueue<>((a, b) -> {
-            if (a.oranges != b.oranges) {
-                return a.oranges - b.oranges;
-            }
-            return a.cost - b.cost;
-        });
-        q.add(new Item(0, n));
-        int best = Integer.MAX_VALUE;
-        while (!q.isEmpty()) {
-            var item = q.poll();
-            int oranges = item.oranges;
-            int cost = item.cost;
-            if (cost > best) {
-                continue;
-            }
-            if (oranges == 0) {
-                best = Math.min(best, cost);
-            }
-            if (visited.getOrDefault(oranges, Integer.MAX_VALUE) <= cost) {
-                continue;
-            }
-            visited.put(oranges, cost);
-            
-            // eat 1
-            q.add(new Item(cost + 1, oranges - 1));
-            
-            // eat half
-            if (oranges % 2 == 0) {
-                q.add(new Item(cost + 1, oranges / 2));
-            }
-            
-            // eat two thirds
-            if (oranges % 3 == 0) {
-                q.add(new Item(cost + 1, oranges / 3));
-            }
+        Map<Integer, Integer> dp = new HashMap<>();
+        dp.put(0, 0);
+        dp.put(1, 1);
+        return solve(n, dp);
+    }
+    
+    private int solve(int n, Map<Integer, Integer> dp) {
+        Integer cached = dp.get(n);
+        if (cached != null) {
+            return cached;
         }
-        return best;
+        int sub1 = solve(n / 2, dp) + 1 + (n % 2);
+        int sub2 = solve(n / 3, dp) + 1 + (n % 3);
+        int result = Math.min(sub1, sub2);
+        dp.put(n, result);
+        return result;
     }
-    
-    private record Item(int cost, int oranges) {
-    }
-    
 }
