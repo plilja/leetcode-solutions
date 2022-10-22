6de98d6c08f0ef821c6dab8fbcd547ff18ba6109
Patrik Lilja <lilja.patrik@gmail.com>
2022-10-22 17:51:31 +0200

    Time: 28 ms (97.67%), Space: 53.8 MB (98.93%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -3,19 +3,13 @@ class Solution {
         PriorityQueue<Item> pq = new PriorityQueue<>((a, b) -> {
             return a.sum - b.sum;
         });
-        pq.add(new Item(nums1[0] + nums2[0], 0, 0));
-        Set<Item> visited = new HashSet<>();
         List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums1.length; ++i) {
+            pq.add(new Item(nums1[i] + nums2[0], i, 0));
+        }
         while (result.size() < k && !pq.isEmpty()) {
             Item item = pq.poll();
-            if (visited.contains(item)) {
-                continue;
-            }
-            visited.add(item);
             result.add(List.of(nums1[item.index1], nums2[item.index2]));
-            if (item.index1 + 1 < nums1.length) {
-                pq.add(new Item(nums1[item.index1 + 1] + nums2[item.index2], item.index1 + 1, item.index2));
-            }
             if (item.index2 + 1 < nums2.length) {
                 pq.add(new Item(nums1[item.index1] + nums2[item.index2 + 1], item.index1, item.index2 + 1));
             }
