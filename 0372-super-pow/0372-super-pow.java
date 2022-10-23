620e6c523cd94b4ebb9dfdc80937da0be156329c
Patrik Lilja <lilja.patrik@gmail.com>
2022-10-23 10:41:23 +0200

    Time: 15 ms (36.26%), Space: 45.2 MB (76.61%) - LeetHub

--- 0372-super-pow/0372-super-pow.java
+++ 0372-super-pow/0372-super-pow.java
@@ -4,24 +4,19 @@ class Solution {
     private static final int MOD = 1337;
     
     public int superPow(int a, int[] b) {
-        StringBuilder sb = new StringBuilder();
-        for (int d : b) {
-            sb.append(d);
+        long acc = 1;
+        for (int i = 0; i < b.length; ++i) {
+            int d = b[i];
+            long accPow10 = 1;
+            for (int j = 0; j < 10; ++j) {
+                accPow10 = accPow10 * acc % MOD;
+            }
+            acc = accPow10;
+            for (int j = 0; j < d; ++j) {
+                acc = acc * ((long)a) % MOD;
+            }
         }
-        return solve(a % MOD, new BigInteger(sb.toString()));
+        return (int) acc;
     }
     
-    private int solve(int a, BigInteger b) {
-        if (BigInteger.ZERO.equals(b)) {
-            return 1;
-        } else if (BigInteger.ONE.equals(b)) {
-            return a;
-        } else if (b.mod(BigInteger.TWO).equals(BigInteger.ZERO)) {
-            int t = solve(a, b.divide(BigInteger.TWO));
-            return t * t % MOD;
-        } else {
-            int t = solve(a, b.subtract(BigInteger.ONE));
-            return a * t % MOD;
-        }
-    }
 }
