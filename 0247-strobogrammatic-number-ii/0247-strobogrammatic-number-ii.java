45ebe8a0e5f8e6ebb624934f94c87a669b570421
Patrik Lilja <lilja.patrik@gmail.com>
2022-10-24 12:37:09 +0200

    Time: 44 ms (64.36%), Space: 94.1 MB (31.21%) - LeetHub

--- 0247-strobogrammatic-number-ii/0247-strobogrammatic-number-ii.java
+++ 0247-strobogrammatic-number-ii/0247-strobogrammatic-number-ii.java
@@ -12,39 +12,27 @@
 */
 class Solution {
     public List<String> findStrobogrammatic(int n) {
-        if (n == 1) {
+        return helper(n, n);
+    }
+    
+    public List<String> helper(int n, int finalLength) {
+        if (n == 0) {
+            return List.of("");
+        } else if (n == 1) {
             return List.of("0", "1", "8");
-        } else if (n == 2) {
-            return List.of("11", "88", "69", "96");
         } else {
-            Set<String> result = new TreeSet<>();
-            if (n % 2 == 1) {
-                List<String> subOne = findStrobogrammatic(n - 1);
-                for (String sub : subOne) {
-                    int middle = sub.length() / 2;
-                    result.add(sub.substring(0, middle) + "0" + sub.substring(middle, sub.length()));
-                    result.add(sub.substring(0, middle) + "1" + sub.substring(middle, sub.length()));
-                    result.add(sub.substring(0, middle) + "8" + sub.substring(middle, sub.length()));
-                }
-            }
-            List<String> subTwo = findStrobogrammatic(n - 2);
+            List<String> result = new ArrayList<>();
+            List<String> subTwo = helper(n - 2, finalLength);
             for (String sub : subTwo) {
+                if (n != finalLength) {
+                    result.add("0" + sub + "0");
+                }
                 result.add("1" + sub + "1");
                 result.add("8" + sub + "8");
                 result.add("6" + sub + "9");
                 result.add("9" + sub + "6");
             }
-            if (n % 2 == 0) {
-                for (String sub : subTwo) {
-                    int middle = sub.length() / 2;
-                    result.add(sub.substring(0, middle) + "00" + sub.substring(middle, sub.length()));
-                    result.add(sub.substring(0, middle) + "69" + sub.substring(middle, sub.length()));
-                    result.add(sub.substring(0, middle) + "96" + sub.substring(middle, sub.length()));
-                    result.add(sub.substring(0, middle) + "88" + sub.substring(middle, sub.length()));
-                    result.add(sub.substring(0, middle) + "11" + sub.substring(middle, sub.length()));
-                }
-            }
-            return new ArrayList<>(result);
+            return result;
         } 
     }
 }
