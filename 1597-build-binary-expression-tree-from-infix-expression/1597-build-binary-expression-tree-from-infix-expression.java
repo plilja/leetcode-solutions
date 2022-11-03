ff41c97600e8b146f5cea9f8de846792ebde92af
Patrik Lilja <lilja.patrik@gmail.com>
2022-11-03 16:41:41 +0100

    Time: 3 ms (53.74%), Space: 42.1 MB (56.54%) - LeetHub

--- 1597-build-binary-expression-tree-from-infix-expression/1597-build-binary-expression-tree-from-infix-expression.java
+++ 1597-build-binary-expression-tree-from-infix-expression/1597-build-binary-expression-tree-from-infix-expression.java
@@ -1,49 +1,43 @@
 class Solution {
     public Node expTree(String s) {
-        List<Node> nodes = exprList(s);
-        List<Node> plusMinus = evalOperators(nodes, Set.of('*', '/'));
-        List<Node> result = evalOperators(plusMinus, Set.of('+', '-'));
-        return result.get(0);
-    }
-    
-    private List<Node> evalOperators(List<Node> nodes, Set<Character> operators) {
-        List<Node> result = new ArrayList<>();
-        int i = 0;
-        while (i < nodes.size()) {
-            Node node = nodes.get(i);
-            boolean isLeaf = node.left == null && node.right == null;
-            if (isLeaf && operators.contains(node.val)) {
-                Node last = result.get(result.size() - 1);
-                Node next = nodes.get(i + 1);
-                node.left = last;
-                node.right = next;
-                result.set(result.size() - 1, node);
-                i += 2;
-            } else {
-                result.add(node);
-                i++;
-            }
-        }
-        return result;
-    }
-    
-    private List<Node> exprList(String s) {
-        Deque<Integer> leftParen = new ArrayDeque<>();
-        List<Node> list = new ArrayList<>();
+        Deque<Character> q = new ArrayDeque<>();
         for (int i = 0; i < s.length(); ++i) {
-            char c = s.charAt(i);
-            if (c == '(') {
-                leftParen.add(i);
-            } else if (c == ')') {
-                int j = leftParen.pollLast();
-                if (leftParen.isEmpty()) {
-                    Node node = expTree(s.substring(j + 1, i));
-                    list.add(node);
-                }
-            } else if (leftParen.isEmpty()) {
-                list.add(new Node(c));
-            }
+            q.add(s.charAt(i));
         }
-        return list;
+        return parseExpression(q);
+    }
+
+    private Node parseExpression(Deque<Character> q) {
+        Node right = parseTerm(q);
+        if (q.isEmpty() || !List.of('+', '-').contains(q.peekLast())) {
+            return right;
+        } else {
+            char op = q.pollLast();
+            Node left = parseExpression(q);
+            return new Node(op, left, right);
+        }
+    }
+
+    private Node parseTerm(Deque<Character> q) {
+        Node right = parseFactor(q);
+        if (q.isEmpty() || !List.of('*', '/').contains(q.peekLast())) {
+            return right;
+        } else {
+            char op = q.pollLast();
+            Node left = parseTerm(q);
+            return new Node(op, left, right);
+        }
+    }
+
+    private Node parseFactor(Deque<Character> q) {
+        if (q.peekLast() == ')') {
+            q.pollLast(); // consume ')'
+            Node expr = parseExpression(q);
+            q.pollLast(); // consume '('
+            return expr;
+        } else {
+            char d = q.pollLast();
+            return new Node(d);
+        }
     }
 }
