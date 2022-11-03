4b11fb3fd00087493bf6ee54b4f948ab2651adca
Patrik Lilja <lilja.patrik@gmail.com>
2022-11-03 16:49:56 +0100

    Time: 1 ms (98.83%), Space: 40.2 MB (93.22%) - LeetHub

--- 1597-build-binary-expression-tree-from-infix-expression/1597-build-binary-expression-tree-from-infix-expression.java
+++ 1597-build-binary-expression-tree-from-infix-expression/1597-build-binary-expression-tree-from-infix-expression.java
@@ -1,3 +1,16 @@
+/*
+Grammar:
+
+expr = term | add | sub
+sub = expr - term
+add = expr + term
+term = operator | mul | div
+mul = term * operator
+div = term / operator
+operator = number | '(' + expr + ')'
+number = '0' - '9'
+
+*/
 class Solution {
     public Node expTree(String s) {
         Deque<Character> q = new ArrayDeque<>();
@@ -9,28 +22,31 @@ class Solution {
 
     private Node parseExpression(Deque<Character> q) {
         Node right = parseTerm(q);
-        if (q.isEmpty() || !List.of('+', '-').contains(q.peekLast())) {
-            return right;
-        } else {
+        boolean isAddOrSub = !q.isEmpty() && List.of('+', '-').contains(q.peekLast());
+        if (isAddOrSub) {
             char op = q.pollLast();
             Node left = parseExpression(q);
             return new Node(op, left, right);
+        } else {
+            return right;
         }
     }
 
     private Node parseTerm(Deque<Character> q) {
         Node right = parseFactor(q);
-        if (q.isEmpty() || !List.of('*', '/').contains(q.peekLast())) {
-            return right;
-        } else {
+        boolean isMulOrDiv = !q.isEmpty() && List.of('*', '/').contains(q.peekLast());
+        if (isMulOrDiv) {
             char op = q.pollLast();
             Node left = parseTerm(q);
             return new Node(op, left, right);
+        } else {
+            return right;
         }
     }
 
     private Node parseFactor(Deque<Character> q) {
-        if (q.peekLast() == ')') {
+        boolean isParenExpr = q.peekLast() == ')';
+        if (isParenExpr) {
             q.pollLast(); // consume ')'
             Node expr = parseExpression(q);
             q.pollLast(); // consume '('
