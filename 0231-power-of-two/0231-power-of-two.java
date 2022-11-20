6e70f27504955efb3b705e3c2e94a955186155b3
Patrik Lilja <lilja.patrik@gmail.com>
2022-11-20 18:03:24 +0100

    Time: 1 ms (100.00%), Space: 39.5 MB (91.94%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -2,12 +2,7 @@ class Solution {
     public boolean isPowerOfTwo(int n) {
         if (n <= 0) {
             return false;
-        } else if (n == 1) {
-            return true;
-        } else if (n % 2 == 0) {
-            return isPowerOfTwo(n / 2);
-        } else {
-            return false;
-        }
+        } 
+        return (n & (n -1)) == 0;
     }
 }
